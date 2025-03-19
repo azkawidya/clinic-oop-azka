3b268449d533e5aad298249c76a13234aead42e6
azkawidya <161341169+azkawidya@users.noreply.github.com>
2025-03-19 18:38:13 +0700

    guided

---
+++ src/Guided/hargabuku/KelasHarga.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hargabuku;
+
+/**
+ *
+ * @author AZKA_WIDYA
+ */
+public class KelasHarga {
+    public void info(){
+        System.out.println("Harga Buku");
+        System.out.println("Rp. 8000");
+        System.out.println("Rp. 10000");
+        System.out.println("Rp. 12000");
+        System.out.println("Rp. 50000");
+        System.out.println("Rp. 16000");
+        System.out.println("Hi This is from harga");
+    }
+}
